cc0563ab9c5ba57b50d86e089c7cee9f3135e79a
Chadrack Mbuyi Kalala <chadrackmbuyi2@gmail.com>
2021-03-11 07:27:29 +0200

    First commit

---
+++ Calculator/src/za/ac/cput/calculator/Addition.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package za.ac.cput.calculator;
+
+/**
+ *
+ * @author WINDOWS 10 PRO
+ */
+public class Addition {
+
+    public static int intgerSum(int a, int b){
+
+        return a + b;
+}
+
+
+}
